c7ad5f4b614472919d998a2d592ec7f2ddc8b87d
Josh Jones <ohblonddev@gmail.com>
2025-03-09 20:58:32 +0000

    Reformat

--- src/main/java/uk/chaoticgoose/streamresult/ResultGatherers.java
+++ src/main/java/uk/chaoticgoose/streamresult/ResultGatherers.java
@@ -11,7 +11,8 @@ public final class ResultGatherers {
     private ResultGatherers() {
     }
 
-    public static <T, R, E extends Exception> Gatherer<T, ?, StreamResult<R, Cause.Single<E>>> mapFallible(FunctionWithException<T, R, E> mapper, FailureAction failureAction) {
+    public static <T, R, E extends Exception>
+    Gatherer<T, ?, StreamResult<R, Cause.Single<E>>> mapFallible(FunctionWithException<T, R, E> mapper, FailureAction failureAction) {
         class State {
             private boolean hasFailed = false;
         }
@@ -30,15 +31,29 @@ public final class ResultGatherers {
         });
     }
 
-    public static <T, R, E1 extends Exception, E2 extends Exception> Gatherer<StreamResult<T, Cause.Single<E1>>, ?, StreamResult<R, Cause.Double<E1, E2>>> mapFallible2(FunctionWithException<T, R, E2> mapper, FailureAction failureAction) {
+    public static <T, R, E1 extends Exception, E2 extends Exception>
+    Gatherer<StreamResult<T, Cause.Single<E1>>, ?, StreamResult<R, Cause.Double<E1, E2>>> mapFallible2(
+        FunctionWithException<T, R, E2> mapper,
+        FailureAction failureAction
+    ) {
         return mapFallibleN(mapper, Cause.Double::fromSingle, Cause.Double::ofSecond, failureAction);
     }
 
-    public static <T, R, E1 extends Exception, E2 extends Exception, E3 extends Exception> Gatherer<StreamResult<T, Cause.Double<E1, E2>>, ?, StreamResult<R, Cause.Triple<E1, E2, E3>>> mapFallible3(FunctionWithException<T, R, E3> mapper, FailureAction failureAction) {
+    public static <T, R, E1 extends Exception, E2 extends Exception, E3 extends Exception>
+    Gatherer<StreamResult<T, Cause.Double<E1, E2>>, ?, StreamResult<R, Cause.Triple<E1, E2, E3>>> mapFallible3(
+        FunctionWithException<T, R, E3> mapper,
+        FailureAction failureAction
+    ) {
         return mapFallibleN(mapper, Cause.Triple::fromDouble, Cause.Triple::ofThird, failureAction);
     }
 
-    private static <T, R, E extends Exception, C1 extends Cause, C2 extends Cause> Gatherer<StreamResult<T, C1>, ?, StreamResult<R, C2>> mapFallibleN(FunctionWithException<T, R, E> mapper, Function<C1, C2> causeUpgrade, Function<E, C2> causeFactory, FailureAction failureAction) {
+    private static <T, R, E extends Exception, C1 extends Cause, C2 extends Cause>
+    Gatherer<StreamResult<T, C1>, ?, StreamResult<R, C2>> mapFallibleN(
+        FunctionWithException<T, R, E> mapper,
+        Function<C1, C2> causeUpgrade,
+        Function<E, C2> causeFactory,
+        FailureAction failureAction
+    ) {
         class State {
             private boolean hasFailed = false;
         }
