164d1b6a3f8d73eb01e21207351ed89b4ccda86d
Josh Jones <ohblonddev@gmail.com>
2025-06-08 14:49:23 +0100

    Add extra null safety test

--- src/test/java/uk/chaoticgoose/streamresult/JSpecifyEnforcementTest.java
+++ src/test/java/uk/chaoticgoose/streamresult/JSpecifyEnforcementTest.java
@@ -2,20 +2,24 @@ package uk.chaoticgoose.streamresult;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
-import com.tngtech.archunit.lang.ArchRule;
 import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 import org.junit.jupiter.api.Test;
 
 import static com.tngtech.archunit.core.importer.ImportOption.Predefined.DO_NOT_INCLUDE_TESTS;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noMethods;
 
 public class JSpecifyEnforcementTest {
+    private static final JavaClasses CLASSES = new ClassFileImporter().withImportOption(DO_NOT_INCLUDE_TESTS).importPackages("uk.chaoticgoose.streamresult");
 
     @Test
     void annotateClassesWithNullMarked() {
-        JavaClasses classes = new ClassFileImporter().withImportOption(DO_NOT_INCLUDE_TESTS).importPackages("uk.chaoticgoose.streamresult");
-        ArchRule rule = classes().should().beAnnotatedWith(NullMarked.class);
+        classes().should().beAnnotatedWith(NullMarked.class).check(CLASSES);
+    }
 
-        rule.check(classes);
+    @Test
+    void publicMethodsShouldNotReturnNullable() {
+        noMethods().that().arePublic().or().arePackagePrivate().should().beAnnotatedWith(Nullable.class).check(CLASSES);
     }
 }
